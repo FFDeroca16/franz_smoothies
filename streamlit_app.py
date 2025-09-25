77d635704b424b0a72e531bd44ab9d422395e164
FFDeroca16 <130569529+FFDeroca16@users.noreply.github.com>
2025-09-25 14:11:22 +0800

    Update streamlit_app.py
    Connected changes need because of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Variables
@@ -13,7 +13,7 @@ st.write(
     Choose the fruits you want in your custom Smoothie
   """
 )
-
+cnx = st.connection("snowflake")
 # option = st.selectbox(
 #     "What is your favorite fruit?",
 #     ("Banana", "Strawberries", "Peaches"),
@@ -30,7 +30,8 @@ st.write(f"The name on your Smoothie will be: {name_on_order}")
 
 
 
-session = get_active_session()
+#session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
