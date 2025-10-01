91f1321a8d00d77a60d1f7edbc9721712dab965d
FFDeroca16 <130569529+FFDeroca16@users.noreply.github.com>
2025-10-01 15:55:45 +0800

    Update streamlit_app.py
    Modified the response to use the search_on variable instead the fruit_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if ingredients_list:
         st.write(f'The search value for {fruit_chosen} is {search_on}.')
       
         st.subheader(f'{fruit_chosen} Nutrition Information')
-        smoothiefroot_response = requests.get(f'https://my.smoothiefroot.com/api/fruit/{fruit_chosen}')
+        smoothiefroot_response = requests.get(f'https://my.smoothiefroot.com/api/fruit/{search_on}')
         smoothiefroot_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
